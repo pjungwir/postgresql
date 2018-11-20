b34ddc71eb5e5a114d4b3dc05f0e5eb0b8a7ff47
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2018-11-19 20:11:33 -0800

    Added WITHOUT OVERLAPS to bison grammar for PKs
    Progress defining an exclusion index behind the temporal PK constraint:
    
    - Create table tests pass.
    - Inserts fail: I think Postgres is confused how a PK can look like an
      exclusion constraint.
    - No PERIOD support yet since they don't exist.
    
    Another WITHOUT OVERLAPS + range test
    
    Teach pg_get_constraintdef about WITH OVERLAPS in primary keys
    
    Let exclusion indexes find a PK WITHOUT OVERLAPS so INSERTs work
    
    Fix sanity_check test that sees without_overlaps_test
    
    Added test for dropping a WITHOUT OVERLAPS PK
    
    pg_dump sql: PK WITHOUT OVERLAPS
    
    Temporal PKs for ALTER TABLE ADD CONSTRAINT
    
    Added PK WITHOUT OVERLAPS tests re constraint dependencies; added TODO comment; fixed whitespace
    
    Fixed some whitespace; added psql \d comment-question
    
    Put WITHOUT OVERLAPS in the right place
    
    Add without_overlaps regression test to the test schedule
    
    Another tabs/spaces fix
    
    Parser for temporal FKs, no impl yet
    
    Add temporal foreign key to the catalog
    
    Force the range in a temporal PK to NOT NULL
    
    Working temporal FK checks when INSERTing in foreign table
    
    Working temporal FK checks when UPDATing a foreign table
    
    Validate temporal FK when it is created
    
    temporal FK check ON DELETE RESTRICT
    
    temporal FK check ON UPDATE NO ACTION/RESTRICT
    
    Removed some out-of-date TODO comments
    
    Document temporal PKs and FKs
    
    Minor cleanup
    
    Add FK optimization: sometimes skip check even when ranges change
    
    Fix to failing tests
    
    Fix typo and line lengths
    
    Incorporate reviewer feedback
    
    Post-merge fixes
    
    Don't use single-element arrays for period attnum/typoids

--- src/backend/catalog/index.c
+++ src/backend/catalog/index.c
@@ -1983,6 +1983,8 @@ index_constraint_create(Relation heapRelation,
 								   islocal,
 								   inhcount,
 								   noinherit,
+									 // TODO: set this for PKs too
+								   false,	/* contemporal */
 								   is_internal);
 
 	/*
--- src/backend/catalog/pg_constraint.c
+++ src/backend/catalog/pg_constraint.c
@@ -75,6 +75,7 @@ CreateConstraintEntry(const char *constraintName,
 					  bool conIsLocal,
 					  int conInhCount,
 					  bool conNoInherit,
+					  bool conTemporal,
 					  bool is_internal)
 {
 	Relation	conDesc;
@@ -185,6 +186,7 @@ CreateConstraintEntry(const char *constraintName,
 	values[Anum_pg_constraint_conislocal - 1] = BoolGetDatum(conIsLocal);
 	values[Anum_pg_constraint_coninhcount - 1] = Int32GetDatum(conInhCount);
 	values[Anum_pg_constraint_connoinherit - 1] = BoolGetDatum(conNoInherit);
+	values[Anum_pg_constraint_contemporal - 1] = BoolGetDatum(conTemporal);
 
 	if (conkeyArray)
 		values[Anum_pg_constraint_conkey - 1] = PointerGetDatum(conkeyArray);
