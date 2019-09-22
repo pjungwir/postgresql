8ddbaf6e1719177345956fec739ac5e6d28addb7
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2019-09-21 21:28:20 -0700

    multirange operators and functions

--- src/backend/parser/parse_coerce.c
+++ src/backend/parser/parse_coerce.c
@@ -1917,6 +1917,8 @@ enforce_generic_type_consistency(const Oid *actual_arg_types,
 							   format_type_be(range_typeid),
 							   format_type_be(elem_typeid))));
 		}
+		else
+			range_typelem = InvalidOid;
 	}
 
 	/* Get the range type based on the multirange type, if we have one */
