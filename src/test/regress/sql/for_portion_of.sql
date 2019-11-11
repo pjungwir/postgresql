e7769cb4fb7520f6db85e206c0527848f9ec7740
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2019-11-10 20:54:43 -0800

    Construct a list of TargetEntrys to set the range

--- src/test/regress/sql/for_portion_of.sql
+++ src/test/regress/sql/for_portion_of.sql
@@ -14,3 +14,12 @@ SET id = '[5,6)'
 WHERE id = '[3,4)';
 
 SELECT * FROM without_overlaps_test;
+
+UPDATE without_overlaps_test
+FOR PORTION OF valid_at FROM '2018-06-01' TO 'infinity'
+-- too add a non PK column so we have something to update:
+SET id = '[6,7)',
+    valid_at = '[1990-01-01,1999-01-01)'
+WHERE id = '[5,6)';
+
+SELECT * FROM without_overlaps_test;
