bf45d7a72d1e0e0d5165a54125a1deca8c4be731
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2019-11-11 21:47:06 -0800

    Small refactor for FOR PORTION OF target entries; make sure to record that the temporal bounds column(s) are changing so we get the right lock and call the right triggers

--- src/backend/parser/analyze.c
+++ src/backend/parser/analyze.c
@@ -75,7 +75,8 @@ static void determineRecursiveColTypes(ParseState *pstate,
 static Query *transformUpdateStmt(ParseState *pstate, UpdateStmt *stmt);
 static List *transformReturningList(ParseState *pstate, List *returningList);
 static List *transformUpdateTargetList(ParseState *pstate,
-									   List *targetList);
+									   List *targetList,
+									   ForPortionOfExpr *forPortionOf);
 static Query *transformDeclareCursorStmt(ParseState *pstate,
 										 DeclareCursorStmt *stmt);
 static Query *transformExplainStmt(ParseState *pstate,
@@ -1050,7 +1051,7 @@ transformOnConflictClause(ParseState *pstate,
 		 * Now transform the UPDATE subexpressions.
 		 */
 		onConflictSet =
-			transformUpdateTargetList(pstate, onConflictClause->targetList);
+			transformUpdateTargetList(pstate, onConflictClause->targetList, NULL);
 
 		onConflictWhere = transformWhereClause(pstate,
 											   onConflictClause->whereClause,
@@ -2452,16 +2453,7 @@ transformUpdateStmt(ParseState *pstate, UpdateStmt *stmt)
 	 * Now we are done with SELECT-like processing, and can get on with
 	 * transforming the target list to match the UPDATE target columns.
 	 */
-	qry->targetList = transformUpdateTargetList(pstate, stmt->targetList);
-	if (stmt->forPortionOf)
-	{
-		ListCell *tl;
-		foreach(tl, qry->forPortionOf->rangeSet)
-		{
-			TargetEntry *tle = (TargetEntry *) lfirst(tl);
-			qry->targetList = lappend(qry->targetList, tle);
-		}
-	}
+	qry->targetList = transformUpdateTargetList(pstate, stmt->targetList, qry->forPortionOf);
 
 	qry->rtable = pstate->p_rtable;
 	qry->jointree = makeFromExpr(pstate->p_joinlist, qual);
@@ -2479,7 +2471,7 @@ transformUpdateStmt(ParseState *pstate, UpdateStmt *stmt)
  *	handle SET clause in UPDATE/INSERT ... ON CONFLICT UPDATE
  */
 static List *
-transformUpdateTargetList(ParseState *pstate, List *origTlist)
+transformUpdateTargetList(ParseState *pstate, List *origTlist, ForPortionOfExpr *forPortionOf)
 {
 	List	   *tlist = NIL;
 	RangeTblEntry *target_rte;
@@ -2548,6 +2540,23 @@ transformUpdateTargetList(ParseState *pstate, List *origTlist)
 		elog(ERROR, "UPDATE target count mismatch --- internal error");
 
 	fill_extraUpdatedCols(target_rte, tupdesc);
+
+	/*
+	 * Record in extraUpdatedCols the temporal bounds if using FOR PORTION OF.
+	 * Since these are part of the primary key this ensures we get the right lock type,
+	 * and it also tells column-specific triggers on that columns to fire.
+	 */
+	if (forPortionOf)
+	{
+		foreach(tl, forPortionOf->rangeSet)
+		{
+			TargetEntry *tle = (TargetEntry *) lfirst(tl);
+			// TODO: I probably don't want to do this until after rewriting:
+			tlist = lappend(tlist, tle);
+			target_rte->extraUpdatedCols = bms_add_member(target_rte->extraUpdatedCols,
+														  tle->resno - FirstLowInvalidHeapAttributeNumber);
+		}
+	}
 
 	return tlist;
 }
