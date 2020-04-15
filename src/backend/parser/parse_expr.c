d46875a6c6aae1bbbee15b12dee8247a9cb6c3fb
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2020-04-14 17:23:28 -0700

    Fix missing EXPR_KIND_UPDATE_PORTION from some switch statements

--- src/backend/parser/parse_expr.c
+++ src/backend/parser/parse_expr.c
@@ -576,6 +576,9 @@ transformColumnRef(ParseState *pstate, ColumnRef *cref)
 		case EXPR_KIND_PARTITION_BOUND:
 			err = _("cannot use column reference in partition bound expression");
 			break;
+		case EXPR_KIND_UPDATE_PORTION:
+			err = _("cannot use column reference in FOR PORTION OF expression");
+			break;
 
 			/*
 			 * There is intentionally no default: case here, so that the
@@ -1888,6 +1891,9 @@ transformSubLink(ParseState *pstate, SubLink *sublink)
 			break;
 		case EXPR_KIND_GENERATED_COLUMN:
 			err = _("cannot use subquery in column generation expression");
+			break;
+		case EXPR_KIND_UPDATE_PORTION:
+			err = _("cannot use subquery in FOR PORTION OF expression");
 			break;
 
 			/*
