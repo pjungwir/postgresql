91698b58c974ec3b671a649f646d006be55b799b
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2021-01-09 21:59:43 -0800

    Add PERIODs
    - Added parsing for SQL:2011 syntax to define an application-time PERIOD on a
      table (in both CREATE TABLE and ALTER TABLE). Make sure we create the PERIOD
      after columns are known (since PERIODs can refer to them) but before
      constraints are handled (since PERIODs can appear in them).
    - Added ALTER TABLE DROP support for PERIODs.
    - Created postgres.pg_period table.
    - Created information_schema.periods view.
    - Added pg_dump support.
    - Added tests and documentation.
    - Automatically define a constraint for each PERIOD requiring the start column
      to be less than the end column.
    - When creating a PERIOD, choose an appropriate range type we can use to
      implement PERIOD-related operations. You can choose one explicitly if there
      is ambiguity (due to multiple range types created over the same base type).

--- src/backend/catalog/aclchk.c
+++ src/backend/catalog/aclchk.c
@@ -2823,6 +2823,7 @@ aclcheck_error(AclResult aclerr, ObjectType objtype,
 					case OBJECT_DEFAULT:
 					case OBJECT_DEFACL:
 					case OBJECT_DOMCONSTRAINT:
+					case OBJECT_PERIOD:
 					case OBJECT_PUBLICATION_NAMESPACE:
 					case OBJECT_PUBLICATION_REL:
 					case OBJECT_ROLE:
@@ -2964,6 +2965,7 @@ aclcheck_error(AclResult aclerr, ObjectType objtype,
 					case OBJECT_DEFACL:
 					case OBJECT_DOMCONSTRAINT:
 					case OBJECT_PARAMETER_ACL:
+					case OBJECT_PERIOD:
 					case OBJECT_PUBLICATION_NAMESPACE:
 					case OBJECT_PUBLICATION_REL:
 					case OBJECT_ROLE:
