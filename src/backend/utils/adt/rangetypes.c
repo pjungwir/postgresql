6957121df7a7dc21243e01603476c0f0e10cf534
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2021-05-23 07:47:20 -0700

    Fixes for ON UPDATE/DELETE CASCADE

--- src/backend/utils/adt/rangetypes.c
+++ src/backend/utils/adt/rangetypes.c
@@ -2130,6 +2130,28 @@ make_empty_range(TypeCacheEntry *typcache)
 	return make_range(typcache, &lower, &upper, true);
 }
 
+char *
+range_as_string(RangeType *r)
+{
+  int16       typlen;
+  bool        typbyval;
+  char        typalign;
+  char        typdelim;
+  Oid typioparam;
+  Oid range_out_oid;
+  get_type_io_data(
+		  RangeTypeGetOid(r),
+		  IOFunc_output,
+		  &typlen,
+		  &typbyval,
+		  &typalign,
+		  &typdelim,
+		  &typioparam,
+		  &range_out_oid);
+  char *rangeStr = OidOutputFunctionCall(range_out_oid, RangeTypePGetDatum(r));
+  return rangeStr;
+}
+
 
 /*
  *----------------------------------------------------------
