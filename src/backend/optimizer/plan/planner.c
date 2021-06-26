ee09e8c09935744b6c73d5701e9e5bd10d1a7d47
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2021-06-25 18:54:35 -0700

    Add UPDATE/DELETE FOR PORTION OF
    This is an extension of the UPDATE and DELETE commands to do a "temporal
    update/delete" based on a range or multirange column. The user can say UPDATE t
    FOR PORTION OF valid_at FROM '2001-01-01' TO '2002-01-01' SET ... (or likewise
    with DELETE) where valid_at is a range or multirange column.
    
    The command is automatically limited to rows overlapping the targeted
    portion, and only history within those bounds is changed. If a row
    represents history partly inside and partly outside the bounds, then
    the command truncates the row's application time to fit within the targeted
    portion, then it inserts one or more "temporal leftovers": new rows
    containing all the original values, except with the application-time
    column changed to only represent the untouched part of history.
    
    To compute the temporal leftovers that are required, we use the *_minus_multi
    set-returning functions defined in 5eed8ce50c.
    
    - Added bison support for FOR PORTION OF syntax. The bounds must be
      constant, so we forbid column references, subqueries, etc. We do
      accept functions like NOW().
    - Added logic to executor to insert new rows for the "temporal leftover"
      part of a record touched by a FOR PORTION OF query.
    - Documented FOR PORTION OF.
    - Added tests.
    
    Author: Paul A. Jungwirth <pj@illuminatedcomputing.com>

--- src/backend/optimizer/plan/planner.c
+++ src/backend/optimizer/plan/planner.c
@@ -2209,6 +2209,7 @@ grouping_planner(PlannerInfo *root, double tuple_fraction,
 										parse->onConflict,
 										mergeActionLists,
 										mergeJoinConditions,
+										parse->forPortionOf,
 										assign_special_exec_param(root));
 		}
 
