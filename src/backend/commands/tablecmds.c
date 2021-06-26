287956555de66f4c8c9c62bf32a8f491818631b1
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2021-06-25 18:54:35 -0700

    Add UPDATE/DELETE FOR PORTION OF
    - Added bison support for FOR PORTION OF syntax. The bounds must be
      constant, so we forbid column references, subqueries, etc. But we
      permit the magic UNBOUNDED keyword as the FROM or the TO (or both)
      to perform an unbounded update/delete. We also accept functions like
      NOW().
    - Added logic to executor to insert new rows for the "leftover" part of
      a record touched by a FOR PORTION OF query.
    - Added tg_temporal descriptor to the TriggerData struct that we pass to
      trigger functions. Our triggers use this to learn what bounds were
      given in the FOR PORTION OF clause.
    - Documented FOR PORTION OF.
    - Documented tg_temporal struct.
    - Added tests.

--- src/backend/commands/tablecmds.c
+++ src/backend/commands/tablecmds.c
@@ -10229,11 +10229,12 @@ ATAddForeignKeyConstraint(List **wqueue, AlteredTableInfo *tab, Relation rel,
 		Oid			periodoperoid;
 		Oid			aggedperiodoperoid;
 		Oid			intersectoperoid;
+		Oid			intersectprocoid;
 		Oid			withoutoverlapsoid;
 
 		FindFKPeriodOpersAndProcs(opclasses[numpks - 1], &periodoperoid,
 								  &aggedperiodoperoid, &intersectoperoid,
-								  &withoutoverlapsoid);
+								  &intersectprocoid, &withoutoverlapsoid);
 	}
 
 	/* First, create the constraint catalog entry itself. */
@@ -12601,6 +12602,7 @@ validateForeignKeyConstraint(char *conname,
 		trigdata.tg_trigtuple = ExecFetchSlotHeapTuple(slot, false, NULL);
 		trigdata.tg_trigslot = slot;
 		trigdata.tg_trigger = &trig;
+		trigdata.tg_temporal = NULL;
 
 		fcinfo->context = (Node *) &trigdata;
 
