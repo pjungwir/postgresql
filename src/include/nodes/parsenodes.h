b55f5fb43589440932d960bfcec227c75065429e
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2021-06-25 18:54:35 -0700

    Add UPDATE/DELETE FOR PORTION OF
    - Added bison support for FOR PORTION OF syntax. The bounds must be
      constant, so we forbid column references, subqueries, etc. We do
      accept functions like NOW().
    - Added logic to executor to insert new rows for the "leftover" part of
      a record touched by a FOR PORTION OF query.
    - Added tg_temporal descriptor to the TriggerData struct that we pass to
      trigger functions. Our foreign key triggers use this to learn what
      bounds were given in the FOR PORTION OF clause.
    - Documented FOR PORTION OF.
    - Documented tg_temporal struct.
    - Added tests.
    
    Author: Paul Jungwirth

--- src/include/nodes/parsenodes.h
+++ src/include/nodes/parsenodes.h
@@ -142,6 +142,9 @@ typedef struct Query
 	 */
 	int			resultRelation pg_node_attr(query_jumble_ignore);
 
+	/* FOR PORTION OF clause for UPDATE/DELETE */
+	ForPortionOfExpr *forPortionOf;
+
 	/* has aggregates in tlist or havingQual */
 	bool		hasAggs pg_node_attr(query_jumble_ignore);
 	/* has window functions in tlist */
@@ -1597,6 +1600,21 @@ typedef struct RowMarkClause
 } RowMarkClause;
 
 /*
+ * ForPortionOfClause
+ *		representation of FOR PORTION OF <period-name> FROM <ts> TO <te>
+ *		or FOR PORTION OF <period-name> (<target>)
+ */
+typedef struct ForPortionOfClause
+{
+	NodeTag		type;
+	char	   *range_name;
+	int			location;
+	Node	   *target;
+	Node	   *target_start;
+	Node	   *target_end;
+} ForPortionOfClause;
+
+/*
  * WithClause -
  *	   representation of WITH clause
  *
@@ -2113,6 +2131,7 @@ typedef struct DeleteStmt
 	WithClause *withClause;		/* WITH clause */
 	ParseLoc	stmt_location;	/* start location, or -1 if unknown */
 	ParseLoc	stmt_len;		/* length in bytes; 0 means "rest of string" */
+	ForPortionOfClause *forPortionOf;	/* FOR PORTION OF clause */
 } DeleteStmt;
 
 /* ----------------------
@@ -2130,6 +2149,7 @@ typedef struct UpdateStmt
 	WithClause *withClause;		/* WITH clause */
 	ParseLoc	stmt_location;	/* start location, or -1 if unknown */
 	ParseLoc	stmt_len;		/* length in bytes; 0 means "rest of string" */
+	ForPortionOfClause *forPortionOf;	/* FOR PORTION OF clause */
 } UpdateStmt;
 
 /* ----------------------
