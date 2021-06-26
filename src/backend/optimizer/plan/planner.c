b40d966c221199e3e460fc3e50a141f9d9ddb241
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2021-06-25 18:54:35 -0700

    Add UPDATE/DELETE FOR PORTION OF
    - Added bison support for FOR PORTION OF syntax. The bounds must be
      constant, so we forbid column references, subqueries, etc. But we
      permit the magic UNBOUNDED keyword as the FROM or the TO (or both)
      to perform an unbounded update/delete. We also accept functions like
      NOW().
    - Added logic to executor to insert new rows for the "leftover" part of
      a record touched by a FOR PORTION OF query.
    - Added tg_temporal descriptor to the TriggerData struct that we pass to
      trigger functions. Our triggers use this to learn what bounds were
      given in the FOR PORTION OF clause.
    - Documented FOR PORTION OF.
    - Documented tg_temporal struct.
    - Added tests.

--- src/backend/optimizer/plan/planner.c
+++ src/backend/optimizer/plan/planner.c
@@ -2047,6 +2047,7 @@ grouping_planner(PlannerInfo *root, double tuple_fraction,
 										parse->onConflict,
 										mergeActionLists,
 										mergeJoinConditions,
+										parse->forPortionOf,
 										assign_special_exec_param(root));
 		}
 
