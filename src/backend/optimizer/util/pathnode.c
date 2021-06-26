7ba3ba1c94206cfbb3710749dda2f1e7a4638124
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2021-06-25 18:54:35 -0700

    Add UPDATE/DELETE FOR PORTION OF
    - Added bison support for FOR PORTION OF syntax. The bounds must be
      constant, so we forbid column references, subqueries, etc. We do
      accept functions like NOW().
    - Added logic to executor to insert new rows for the "temporal leftover"
      part of a record touched by a FOR PORTION OF query.
    - Documented FOR PORTION OF.
    - Added tests.
    
    Author: Paul A. Jungwirth <pj@illuminatedcomputing.com>

--- src/backend/optimizer/util/pathnode.c
+++ src/backend/optimizer/util/pathnode.c
@@ -3633,7 +3633,7 @@ create_modifytable_path(PlannerInfo *root, RelOptInfo *rel,
 						List *withCheckOptionLists, List *returningLists,
 						List *rowMarks, OnConflictExpr *onconflict,
 						List *mergeActionLists, List *mergeJoinConditions,
-						int epqParam)
+						ForPortionOfExpr *forPortionOf, int epqParam)
 {
 	ModifyTablePath *pathnode = makeNode(ModifyTablePath);
 
@@ -3699,6 +3699,7 @@ create_modifytable_path(PlannerInfo *root, RelOptInfo *rel,
 	pathnode->returningLists = returningLists;
 	pathnode->rowMarks = rowMarks;
 	pathnode->onconflict = onconflict;
+	pathnode->forPortionOf = forPortionOf;
 	pathnode->epqParam = epqParam;
 	pathnode->mergeActionLists = mergeActionLists;
 	pathnode->mergeJoinConditions = mergeJoinConditions;
