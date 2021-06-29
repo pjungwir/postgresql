90acd78e053c16eed14b73a62d983aca13a903e9
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2021-06-28 17:33:27 -0700

    Add temporal PRIMARY KEY and UNIQUE constraints
    - Added WITHOUT OVERLAPS to the bison grammar. We permit either range
      columns or PERIODs.
    - Temporal PRIMARY KEYs and UNIQUE constraints are backed by GiST
      indexes instead of B-tree indexes, since they are essentially
      exclusion constraints with = for the scalar parts of the key and &&
      for the temporal part. With PERIODs we index a range expression using
      the PERIOD start and end columns. With ranges we can just index the
      column, so no expression is needed.
    - Added pg_index.indperiod to record when a PERIOD is used in an index
      constraint.
    - Added pg_constraint.contemporal to say whether a constraint is a
      temporal constraint.
    - Added pg_constraint.conperiod to record the PERIOD oid if a PERIOD is
      used. For range columns we can just include the column attnum, like
      any other column appearing in a constraint.
    - Added docs and tests.
    - Added pg_dump support.

--- src/backend/nodes/copyfuncs.c
+++ src/backend/nodes/copyfuncs.c
@@ -3083,6 +3083,7 @@ _copyConstraint(const Constraint *from)
 	COPY_SCALAR_FIELD(fk_del_action);
 	COPY_NODE_FIELD(old_conpfeqop);
 	COPY_SCALAR_FIELD(old_pktable_oid);
+	COPY_NODE_FIELD(without_overlaps);
 	COPY_SCALAR_FIELD(skip_validation);
 	COPY_SCALAR_FIELD(initially_valid);
 
@@ -3675,6 +3676,7 @@ _copyIndexStmt(const IndexStmt *from)
 	COPY_SCALAR_FIELD(unique);
 	COPY_SCALAR_FIELD(primary);
 	COPY_SCALAR_FIELD(isconstraint);
+	COPY_SCALAR_FIELD(istemporal);
 	COPY_SCALAR_FIELD(deferrable);
 	COPY_SCALAR_FIELD(initdeferred);
 	COPY_SCALAR_FIELD(transformed);
