612387d08d1b49f064d6122c723754af950e98b3
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2023-07-20 21:20:51 -0700

    fixup periods: Exclude GENERATED column from CREATE TABLE LIKE
    When you use LIKE inside CREATE TABLE to copy columns, we should skip
    the GENERATED columns that implement PERIODs. Because they don't make
    sense without their PERIODs, and LIKE never copies PERIODs. In fact it
    causes problems because then if you use LIKE and also try to add a
    PERIOD of the same name as in the parent table, you get a conflict: not
    between the old & new period, but between the old period's column and
    the new period.

--- src/include/catalog/pg_period.h
+++ src/include/catalog/pg_period.h
@@ -52,5 +52,6 @@ MAKE_SYSCACHE(PERIODNAME, pg_period_perrelid_pername_index, 32);
 extern void RemovePeriodById(Oid periodId);
 
 extern Oid get_relation_period_oid(Oid relid, const char *pername, bool missing_ok);
+extern Bitmapset *get_period_attnos(Oid relid);
 
 #endif							/* PG_PERIOD_H */
