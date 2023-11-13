67be655ef3f0904e9f141dbcef01fce3cde9f58b
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2023-11-12 20:21:32 -0800

    fixup fks: rename contemporal to conwithoutoverlaps

--- src/backend/commands/tablecmds.c
+++ src/backend/commands/tablecmds.c
@@ -212,6 +212,7 @@ typedef struct NewConstraint
 	ConstrType	contype;		/* CHECK or FOREIGN */
 	Oid			refrelid;		/* PK rel, if FOREIGN */
 	Oid			refindid;		/* OID of PK's index, if FOREIGN */
+	bool		conwithperiod;	/* Whether the new FOREIGN KEY uses PERIOD */
 	Oid			conid;			/* OID of pg_constraint entry, if FOREIGN */
 	Node	   *qual;			/* Check expr or CONSTR_FOREIGN Constraint */
 	ExprState  *qualstate;		/* Execution state for CHECK expr */
@@ -5944,7 +5945,7 @@ ATRewriteTables(AlterTableStmt *parsetree, List **wqueue, LOCKMODE lockmode,
 				validateForeignKeyConstraint(fkconstraint->conname, rel, refrel,
 											 con->refindid,
 											 con->conid,
-											 con->contemporal);
+											 con->conwithperiod);
 
 				/*
 				 * No need to mark the constraint row as validated, we did
@@ -10410,7 +10411,7 @@ addFkRecurseReferencing(List **wqueue, Constraint *fkconstraint, Relation rel,
 			newcon->refrelid = RelationGetRelid(pkrel);
 			newcon->refindid = indexOid;
 			newcon->conid = parentConstr;
-			newcon->contemporal = fkconstraint->fk_period != NULL;
+			newcon->conwithperiod = fkconstraint->fk_period != NULL;
 			newcon->qual = (Node *) fkconstraint;
 
 			tab->constraints = lappend(tab->constraints, newcon);
@@ -10797,7 +10798,7 @@ CloneFkReferenced(Relation parentRel, Relation partitionRel)
 							   true,
 							   deleteTriggerOid,
 							   updateTriggerOid,
-							   constrForm->contemporal);
+							   constrForm->conwithoutoverlaps);
 
 		table_close(fkRel, NoLock);
 		ReleaseSysCache(tuple);
@@ -11071,7 +11072,7 @@ CloneFkReferencing(List **wqueue, Relation parentRel, Relation partRel)
 								AccessExclusiveLock,
 								insertTriggerOid,
 								updateTriggerOid,
-								constrForm->contemporal);
+								constrForm->conwithoutoverlaps);
 		table_close(pkrel, NoLock);
 	}
 
