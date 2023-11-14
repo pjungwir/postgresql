06e2e00fd7c68f2ece03691182c90ebf7b95e96d
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2023-11-13 16:26:00 -0800

    fixup fks: add btree_gist test

--- contrib/btree_gist/sql/without_overlaps.sql
+++ contrib/btree_gist/sql/without_overlaps.sql
@@ -23,3 +23,15 @@ INSERT INTO temporal_rng VALUES
 -- should fail:
 INSERT INTO temporal_rng VALUES
   (1, '[2000-06-01,2001-01-01)');
+
+-- Foreign key
+CREATE TABLE temporal_fk_rng2rng (
+  id integer,
+  valid_at tsrange,
+  parent_id integer,
+  CONSTRAINT temporal_fk_rng2rng_pk PRIMARY KEY (id, valid_at WITHOUT OVERLAPS),
+  CONSTRAINT temporal_fk_rng2rng_fk FOREIGN KEY (parent_id, PERIOD valid_at)
+    REFERENCES temporal_rng (id, PERIOD valid_at)
+);
+\d temporal_fk_rng2rng
+SELECT pg_get_constraintdef(oid) FROM pg_constraint WHERE conname = 'temporal_fk_rng2rng_fk';
