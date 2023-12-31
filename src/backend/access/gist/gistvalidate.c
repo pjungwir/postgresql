ede4d930ff14a1770f451e4277580cc1dd5a27bf
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2023-12-30 21:07:28 -0800

    fixup fpo: use AMOP_PORTION not AMOP_OTHER

--- src/backend/access/gist/gistvalidate.c
+++ src/backend/access/gist/gistvalidate.c
@@ -239,9 +239,9 @@ gistvalidate(Oid opclassoid)
 			/* Search operators must always return bool */
 			op_rettype = BOOLOID;
 		}
-		else if (oprform->amoppurpose == AMOP_OTHER)
+		else if (oprform->amoppurpose == AMOP_PORTION)
 		{
-			/* No restrictions on OTHER */
+			/* No restrictions on PORTION */
 			op_rettype = InvalidOid;
 		}
 		else
