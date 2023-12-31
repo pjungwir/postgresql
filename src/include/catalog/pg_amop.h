f318621d270c6538c50a2c40c4c0481bcd572cb3
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2023-12-30 21:07:28 -0800

    fixup fpo: use AMOP_PORTION not AMOP_OTHER

--- src/include/catalog/pg_amop.h
+++ src/include/catalog/pg_amop.h
@@ -96,7 +96,7 @@ DECLARE_UNIQUE_INDEX_PKEY(pg_amop_oid_index, 2756, AccessMethodOperatorOidIndexI
 /* allowed values of amoppurpose: */
 #define AMOP_SEARCH		's'		/* operator is for search */
 #define AMOP_ORDER		'o'		/* operator is for ordering */
-#define AMOP_OTHER		'x'		/* operator is for something else */
+#define AMOP_PORTION	'p'		/* operator is for FOR PORTION OF */
 
 #endif							/* EXPOSE_TO_CLIENT_CODE */
 
