bb7175cb376ad88c287e29a3484ab11d696eb27a
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2024-01-04 20:00:03 -0800

    fixup fks: improve err msg if PK has WITHOUT OVERLAPS and fk doesn't use PERIOD
    from jian he feedback 11/19

--- src/test/regress/sql/without_overlaps.sql
+++ src/test/regress/sql/without_overlaps.sql
@@ -355,6 +355,15 @@ CREATE TABLE temporal_fk_rng2rng (
 	CONSTRAINT temporal_fk_rng2rng_fk FOREIGN KEY (parent_id, valid_at)
 		REFERENCES temporal_rng (id, PERIOD valid_at)
 );
+-- (parent_id, valid_at) REFERENCES [implicit]
+CREATE TABLE temporal_fk_rng2rng (
+	id int4range,
+	valid_at tsrange,
+	parent_id int4range,
+	CONSTRAINT temporal_fk_rng2rng_pk PRIMARY KEY (id, valid_at WITHOUT OVERLAPS),
+	CONSTRAINT temporal_fk_rng2rng_fk FOREIGN KEY (parent_id, valid_at)
+		REFERENCES temporal_rng
+);
 -- (parent_id, PERIOD valid_at) REFERENCES (id)
 CREATE TABLE temporal_fk_rng2rng (
 	id int4range,
