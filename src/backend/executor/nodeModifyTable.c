9c5c218c69c7e4a5deedad2f5a4fb1e19380e498
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2024-01-10 20:55:18 -0800

    fixup fpo: add assert, jian he 8 Jan 2024

--- src/backend/executor/nodeModifyTable.c
+++ src/backend/executor/nodeModifyTable.c
@@ -1284,6 +1284,7 @@ ExecForPortionOfLeftovers(ModifyTableContext *context,
 	deconstruct_array(DatumGetArrayTypeP(allLeftovers), typcache->type_id, typcache->typlen,
 					  typcache->typbyval, typcache->typalign, &leftovers, NULL, &nleftovers);
 
+	Assert(nleftovers >= 0);
 	if (nleftovers > 0)
 	{
 		bool shouldFree = false;
