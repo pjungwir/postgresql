4cc69fa957d946da0380a70b0b7cde6ef45c14f4
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2024-02-13 20:22:44 -0800

    fixup fks: improve CASCADE-not-yet-supported errmsg

--- src/backend/commands/tablecmds.c
+++ src/backend/commands/tablecmds.c
@@ -12796,7 +12796,8 @@ createForeignKeyActionTriggers(Relation rel, Oid refRelOid, Constraint *fkconstr
 			case FKCONSTR_ACTION_SETDEFAULT:
 				ereport(ERROR,
 						(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
-						 errmsg("action not supported for temporal foreign keys")));
+						 errmsg("invalid %s action for foreign key constraint using PERIOD",
+								"ON DELETE")));
 				break;
 			default:
 				elog(ERROR, "unrecognized FK action type: %d",
@@ -12887,7 +12888,8 @@ createForeignKeyActionTriggers(Relation rel, Oid refRelOid, Constraint *fkconstr
 			case FKCONSTR_ACTION_SETDEFAULT:
 				ereport(ERROR,
 						(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
-						 errmsg("action not supported for temporal foreign keys")));
+						 errmsg("invalid %s action for foreign key constraint using PERIOD",
+								"ON UPDATE")));
 				break;
 			default:
 				elog(ERROR, "unrecognized FK action type: %d",
