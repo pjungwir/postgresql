e908fa5fc596bfd152a88ab8f90371315344a404
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2024-02-14 17:12:01 -0800

    fixup fks cascade: fix multirange support and add tests

--- src/backend/commands/tablecmds.c
+++ src/backend/commands/tablecmds.c
@@ -10263,8 +10263,12 @@ ATAddForeignKeyConstraint(List **wqueue, AlteredTableInfo *tab, Relation rel,
 	{
 		Oid			periodoperoid;
 		Oid			aggedperiodoperoid;
+		Oid			intersectprocoid;
 
-		FindFKPeriodOpers(opclasses[numpks - 1], &periodoperoid, &aggedperiodoperoid);
+		FindFKPeriodOpersAndProcs(opclasses[numpks - 1],
+								  &periodoperoid,
+								  &aggedperiodoperoid,
+								  &intersectprocoid);
 	}
 
 	/*
