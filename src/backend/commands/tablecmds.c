52e615aacebc46520d132d00afac9b7e5c5f3f27
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2024-02-20 19:37:31 -0800

    fixup fks: rename conwithoutoverlaps to conperiod

--- src/backend/commands/tablecmds.c
+++ src/backend/commands/tablecmds.c
@@ -10865,7 +10865,7 @@ CloneFkReferenced(Relation parentRel, Relation partitionRel)
 							   true,
 							   deleteTriggerOid,
 							   updateTriggerOid,
-							   constrForm->conwithoutoverlaps);
+							   constrForm->conperiod);
 
 		table_close(fkRel, NoLock);
 		ReleaseSysCache(tuple);
@@ -11074,7 +11074,7 @@ CloneFkReferencing(List **wqueue, Relation parentRel, Relation partRel)
 			fkconstraint->conname = pstrdup(NameStr(constrForm->conname));
 
 		indexOid = constrForm->conindid;
-		is_temporal = constrForm->conwithoutoverlaps;
+		is_temporal = constrForm->conperiod;
 		constrOid =
 			CreateConstraintEntry(fkconstraint->conname,
 								  constrForm->connamespace,
