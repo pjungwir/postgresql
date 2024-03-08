b792ad1f43b98be501ab5dabd43813e59fe86e64
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2024-03-08 13:06:15 -0800

    fixup periods: add fk cascade tests

--- src/backend/commands/tablecmds.c
+++ src/backend/commands/tablecmds.c
@@ -10816,8 +10816,9 @@ ATAddForeignKeyConstraint(List **wqueue, AlteredTableInfo *tab, Relation rel,
 	for (i = 0; i < numfks; i++)
 	{
 		char		attgenerated = TupleDescAttr(RelationGetDescr(rel), fkattnum[i] - 1)->attgenerated;
+		Bitmapset  *periods = get_period_attnos(RelationGetRelid(rel));
 
-		if (attgenerated)
+		if (attgenerated && !bms_is_member(fkattnum[i], periods))
 		{
 			/*
 			 * Check restrictions on UPDATE/DELETE actions, per SQL standard
