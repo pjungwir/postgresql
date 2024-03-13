7eb50348d8fbe715f596879e2563a6507c4736bd
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2024-03-13 11:45:04 -0700

    fixup fpo: change tests to [x,y) format

--- src/test/regress/sql/without_overlaps.sql
+++ src/test/regress/sql/without_overlaps.sql
@@ -679,13 +679,13 @@ WHERE id = '[5,6)' AND valid_at = daterange('2018-01-01', '2018-02-01');
 -- changing an unreferenced part is okay:
 UPDATE temporal_rng
 FOR PORTION OF valid_at FROM '2018-01-02' TO '2018-01-03'
-SET id = '[7,7]'
-WHERE id = '[5,5]';
+SET id = '[7,8)'
+WHERE id = '[5,6)';
 -- changing just a part fails:
 UPDATE temporal_rng
 FOR PORTION OF valid_at FROM '2018-01-05' TO '2018-01-10'
-SET id = '[7,7]'
-WHERE id = '[5,5]';
+SET id = '[7,8)'
+WHERE id = '[5,6)';
 -- then delete the objecting FK record and the same PK update succeeds:
 DELETE FROM temporal_fk_rng2rng WHERE id = '[3,4)';
 UPDATE temporal_rng SET valid_at = daterange('2016-01-01', '2016-02-01')
@@ -723,13 +723,13 @@ WHERE id = '[5,6)' AND valid_at = daterange('2018-01-01', '2018-02-01');
 -- changing an unreferenced part is okay:
 UPDATE temporal_rng
 FOR PORTION OF valid_at FROM '2018-01-02' TO '2018-01-03'
-SET id = '[7,7]'
-WHERE id = '[5,5]';
+SET id = '[7,8)'
+WHERE id = '[5,6)';
 -- changing just a part fails:
 UPDATE temporal_rng
 FOR PORTION OF valid_at FROM '2018-01-05' TO '2018-01-10'
-SET id = '[7,7]'
-WHERE id = '[5,5]';
+SET id = '[7,8)'
+WHERE id = '[5,6)';
 -- then delete the objecting FK record and the same PK update succeeds:
 DELETE FROM temporal_fk_rng2rng WHERE id = '[3,4)';
 UPDATE temporal_rng SET valid_at = daterange('2016-01-01', '2016-02-01')
@@ -760,17 +760,17 @@ DELETE FROM temporal_rng WHERE id = '[5,6)' AND valid_at = daterange('2018-01-01
 -- deleting an unreferenced part is okay:
 DELETE FROM temporal_rng
 FOR PORTION OF valid_at FROM '2018-01-02' TO '2018-01-03'
-WHERE id = '[5,5]';
+WHERE id = '[5,6)';
 -- deleting just a part fails:
 DELETE FROM temporal_rng
 FOR PORTION OF valid_at FROM '2018-01-05' TO '2018-01-10'
-WHERE id = '[5,5]';
+WHERE id = '[5,6)';
 -- then delete the objecting FK record and the same PK delete succeeds:
 DELETE FROM temporal_fk_rng2rng WHERE id = '[3,4)';
 DELETE FROM temporal_rng WHERE id = '[5,6)' AND valid_at = daterange('2018-01-01', '2018-02-01');
 -- clean up:
-DELETE FROM temporal_fk_rng2rng WHERE parent_id = '[5,5]';
-DELETE FROM temporal_rng WHERE id IN ('[5,5]');
+DELETE FROM temporal_fk_rng2rng WHERE parent_id = '[5,6)';
+DELETE FROM temporal_rng WHERE id IN ('[5,6)');
 
 --
 -- test FK referenced deletes RESTRICT
@@ -797,17 +797,17 @@ DELETE FROM temporal_rng WHERE id = '[5,6)' AND valid_at = daterange('2018-01-01
 -- deleting an unreferenced part is okay:
 DELETE FROM temporal_rng
 FOR PORTION OF valid_at FROM '2018-01-02' TO '2018-01-03'
-WHERE id = '[5,5]';
+WHERE id = '[5,6)';
 -- deleting just a part fails:
 DELETE FROM temporal_rng
 FOR PORTION OF valid_at FROM '2018-01-05' TO '2018-01-10'
-WHERE id = '[5,5]';
+WHERE id = '[5,6)';
 -- then delete the objecting FK record and the same PK delete succeeds:
 DELETE FROM temporal_fk_rng2rng WHERE id = '[3,4)';
 DELETE FROM temporal_rng WHERE id = '[5,6)' AND valid_at = daterange('2018-01-01', '2018-02-01');
 -- clean up:
-DELETE FROM temporal_fk_rng2rng WHERE parent_id = '[5,5]';
-DELETE FROM temporal_rng WHERE id IN ('[5,5]');
+DELETE FROM temporal_fk_rng2rng WHERE parent_id = '[5,6)';
+DELETE FROM temporal_rng WHERE id IN ('[5,6)');
 
 --
 -- test ON UPDATE/DELETE options
