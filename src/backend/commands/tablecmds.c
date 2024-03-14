c222462b4175f82ad364ab2421be5f09d83868b7
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2024-03-13 19:51:25 -0700

    fixup fks: fix obsolete comment

--- src/backend/commands/tablecmds.c
+++ src/backend/commands/tablecmds.c
@@ -12207,8 +12207,7 @@ transformFkeyGetPrimaryKey(Relation pkrel, Oid *indexOid,
 
 	/*
 	 * Now build the list of PK attributes from the indkey definition (we
-	 * assume a primary key cannot have expressional elements, unless it
-	 * has a PERIOD)
+	 * assume a primary key cannot have expressional elements)
 	 */
 	*attnamelist = NIL;
 	for (i = 0; i < indexStruct->indnkeyatts; i++)
