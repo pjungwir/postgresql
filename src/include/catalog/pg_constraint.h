361c8a5e39e52cf7611e3319454b1591bdae7aa2
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2024-03-15 17:43:16 -0700

    Support multiranges or any type in temporal FKs
    Use referencedagg GiST support func to aggregate the PK records into one
    object that can be compared to the FK side.

--- src/include/catalog/pg_constraint.h
+++ src/include/catalog/pg_constraint.h
@@ -283,9 +283,10 @@ extern void DeconstructFkConstraintRow(HeapTuple tuple, int *numfks,
 									   AttrNumber *conkey, AttrNumber *confkey,
 									   Oid *pf_eq_oprs, Oid *pp_eq_oprs, Oid *ff_eq_oprs,
 									   int *num_fk_del_set_cols, AttrNumber *fk_del_set_cols);
-extern void FindFKPeriodOpers(Oid opclass,
-							  Oid *periodoperoid,
-							  Oid *aggedperiodoperoid);
+extern void FindFKPeriodOpersAndProcs(Oid opclass,
+									  Oid *periodoperoid,
+									  Oid *aggedperiodoperoid,
+									  Oid *periodprocoid);
 
 extern bool check_functional_grouping(Oid relid,
 									  Index varno, Index varlevelsup,
