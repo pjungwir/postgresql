0e094008373aeb895c246f3efe6fb0ce300f1cf2
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2024-03-15 17:43:01 -0700

    fixup fks: remove referencedagg use

--- src/backend/commands/tablecmds.c
+++ src/backend/commands/tablecmds.c
@@ -10260,9 +10260,8 @@ ATAddForeignKeyConstraint(List **wqueue, AlteredTableInfo *tab, Relation rel,
 	{
 		Oid			periodoperoid;
 		Oid			aggedperiodoperoid;
-		Oid			periodprocoid;
 
-		FindFKPeriodOpersAndProcs(opclasses[numpks - 1], &periodoperoid, &aggedperiodoperoid, &periodprocoid);
+		FindFKPeriodOpers(opclasses[numpks - 1], &periodoperoid, &aggedperiodoperoid);
 	}
 
 	/*
