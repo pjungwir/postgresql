2590304d6dffef6e5f287db6e03fe9c1d59327e5
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2024-03-15 17:43:16 -0700

    Support multiranges in temporal FKs
    Allow multiranges as the PERIOD part of a FOREIGN KEY. We already allow
    range types, and adding multiranges is straightforward.

--- src/backend/commands/tablecmds.c
+++ src/backend/commands/tablecmds.c
@@ -10256,8 +10256,9 @@ ATAddForeignKeyConstraint(List **wqueue, AlteredTableInfo *tab, Relation rel,
 	{
 		Oid			periodoperoid;
 		Oid			aggedperiodoperoid;
+		Oid			periodprocoid;
 
-		FindFKPeriodOpers(opclasses[numpks - 1], &periodoperoid, &aggedperiodoperoid);
+		FindFKPeriodOpersAndProcs(opclasses[numpks - 1], &periodoperoid, &aggedperiodoperoid, &periodprocoid);
 	}
 
 	/*
