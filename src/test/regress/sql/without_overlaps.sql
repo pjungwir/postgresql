fc4ff3f679ed3529ea76b57a253d59bcb876f5e8
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2024-06-26 15:58:03 -0700

    Revert "rm"
    This reverts commit 824dd5a336eaabbbd1fae7eaa88ff6d0c8b2f941.

--- src/test/regress/sql/without_overlaps.sql
+++ src/test/regress/sql/without_overlaps.sql
@@ -305,6 +305,7 @@ SET     id = '[21,22)',
         valid_at = '[2018-01-02,2018-02-03)'
 WHERE   id = '[11,12)'
 AND     valid_at @> '2020-01-15'::date;
+SELECT * FROM temporal_rng ORDER BY id, valid_at;
 -- should fail:
 UPDATE  temporal_rng
 SET     id = '[1,2)',
